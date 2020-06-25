53c918383f834150c48846b9b74bc9b8d9644bfa
Jairo Doni <jairo.doni97@gmail.com>
2020-06-24 21:55:38 -0300

    reduziu o maximo de pulos, adicionou dois inimigos diferentes, adicionou pontuação, mais  ajustes no posicionamento dos elementos em tela

--- scripts/jogo/personagem.js
+++ scripts/jogo/personagem.js
@@ -1,28 +1,37 @@
 class Personagem extends Animacao {
-  constructor(matriz, imagem, x, largura, altura, larguraSprite, alturaSprite) {
-    super(matriz, imagem, x, largura, altura, larguraSprite, alturaSprite);
+  constructor(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite) {
+    super(matriz, imagem, x,variacaoY, largura, altura, larguraSprite, alturaSprite);
 
-    this.yInicial = height - this.altura;
+    this.variacaoY = variacaoY;
+    this.yInicial = height - this.altura - this.variacaoY;
     this.y = this.yInicial;
 
-    this.velocidadeDoPulo = 0;
-    this.gravidade = 3;
+    this.velocidadePulo = 0;
+    this.alturaPulo= -52; 
+    this.gravidade = 6;
+    this.pulos = 0;
+
   }
   pula() {
-    this.velocidadeDoPulo = -30;
+    if(this.pulos < 2){
+      this.velocidadePulo = this.alturaPulo;
+      this.pulos++;
+
+    }
   }
   AplicaGravidade() {
-    this.y = this.y + this.velocidadeDoPulo;
-    this.velocidadeDoPulo = this.velocidadeDoPulo + this.gravidade;
+    this.y = this.y + this.velocidadePulo;
+    this.velocidadePulo = this.velocidadePulo + this.gravidade;
     if (this.y > this.yInicial) {
       this.y = this.yInicial;
+      this.pulos = 0;
     }
   }
 
   estaColidindo(inimigo) {
-    // noFill();
-    // rect(this.x, this.y, this.largura, this.altura);
-    // rect(inimigo.x, inimigo.y, inimigo.largura, inimigo.altura,);
+    //  noFill();
+    //  rect(this.x, this.y, this.largura, this.altura);
+    //  rect(inimigo.x, inimigo.y, inimigo.largura, inimigo.altura,);
     const precisao = .7;
     const colisao =  collideRectRect(
       this.x,
--- sketch.js
+++ sketch.js
@@ -1,6 +1,8 @@
 let imgCenario;
 let imgPersonagem;
 let imgInimigo;
+let imgInimigoGrande;
+let imgInimigoVoador;
 let imgGameOver;
 let somGameOver;
 let somPulo;
@@ -9,7 +11,29 @@ let cenario;
 let somAmbiente;
 let personagem;
 let inimigo;
+let inimigoGrande;
+let inimigoVoador;
+let pontuacao;
 
+
+const matrizPresonagem = [
+  [0, 0],
+  [220, 0],
+  [440, 0],
+  [660, 0],
+  [0, 270],
+  [220, 270],
+  [440, 270],
+  [660, 270],
+  [0, 540],
+  [220, 540],
+  [440, 540],
+  [660, 540],
+  [0, 810],
+  [220, 810],
+  [440, 810],
+  [660, 810],
+];
 const matrizInimigo = [
   [0, 0],
   [105, 0],
@@ -40,29 +64,61 @@ const matrizInimigo = [
   [210, 609],
   [315, 609],
 ];
-const matrizPresonagem = [
+const matrizInimigoGrande = [
   [0, 0],
-  [220, 0],
-  [440, 0],
-  [660, 0],
-  [0, 270],
-  [220, 270],
-  [440, 270],
-  [660, 270],
-  [0, 540],
-  [220, 540],
-  [440, 540],
-  [660, 540],
-  [0, 810],
-  [220, 810],
-  [440, 810],
-  [660, 810],
+  [400, 0],
+  [800, 0],
+  [1200, 0],
+  [1600, 0],
+  [0, 400],
+  [400, 400],
+  [800, 400],
+  [1200, 400],
+  [1600, 400],
+  [0, 800],
+  [400, 800],
+  [800, 800],
+  [1200, 800],
+  [1600, 800],
+  [0, 1200],
+  [400, 1200],
+  [800, 1200],
+  [1200, 1200],
+  [1600, 1200],
+  [0, 1600],
+  [400, 1600],
+  [800, 1600],
+  [1200, 1600],
+  [1600, 1600],
+  [0, 2000],
+  [400, 2000],
+  [800, 2000],
 ];
-
+const matrizInimigoVoador = [
+  [0,0],
+  [200, 0],
+  [400, 0],
+  [0, 150],
+  [200, 150],
+  [400, 150],
+  [0, 300],
+  [200, 300],
+  [400, 300],
+  [0, 450],
+  [200, 450],
+  [400, 450],
+  [0, 600],
+  [200, 600],
+  [400, 600],
+  [0, 750],
+]
+const inimigos=[];
 function preload() {
   imgCenario = loadImage("imagens/cenario/floresta.png");
-  imgPersonagem = loadImage("imagens/personagem/correndo.png");
+  imgPersonagem = loadImage("imagens/personagem/correndo2.png");
   imgInimigo = loadImage("imagens/inimigos/gotinha.png");
+  imgInimigoVoador = loadImage("imagens/inimigos/gotinha-voadora.png");
+  imgInimigoGrande = loadImage("imagens/inimigos/troll.png");
   imgGameOver = loadImage("imagens/assets/game-over.png");
   somAmbiente = loadSound("sons/fullmetal_mixagem02.mp3");
   somPulo = loadSound("sons/somPulo.mp3");
@@ -72,30 +128,64 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
   cenario = new Cenario(imgCenario, 3);
+  pontuacao = new Pontuacao();
   personagem = new Personagem(
     matrizPresonagem,
     imgPersonagem,
     0,
+    30,
     110,
     135,
     220,
     270
   );
-  inimigo = new Inimigo(
+  const inimigo = new Inimigo(
     matrizInimigo,
     imgInimigo,
     width - 52,
+    30,
     52,
     52,
     104,
-    104
+    104,
+    10, 
+    200
   );
+  const inimigoVoador = new Inimigo(
+    matrizInimigoVoador,
+    imgInimigoVoador,
+    width - 52,
+    200,
+    100,
+    75,
+    200,
+    150,
+    10, 
+    1500
+  );
+  const inimigoGrande = new Inimigo(
+    matrizInimigoGrande,
+    imgInimigoGrande,
+    width,
+    0,
+    200,
+    200,
+    400,
+    400,
+    10,
+    2500
+  );
+
+  inimigos.push(inimigo);
+  inimigos.push(inimigoVoador);
+  inimigos.push(inimigoGrande);
+  
   frameRate(40);
-  somAmbiente.loop();
+  //somAmbiente.loop();
 }
 
-function keyPressed(){
-  if(key === ' '){
+function keyPressed() {
+  if (key === " ") {
     personagem.pula();
     somPulo.play();
   }
@@ -104,17 +194,27 @@ function keyPressed(){
 function draw() {
   cenario.exibe();
   cenario.move();
+  pontuacao.exibe();
+  pontuacao.adicionarPonto();
 
   personagem.exibe();
   personagem.AplicaGravidade();
-  
-  inimigo.exibe();
-  inimigo.move()
-  
-  if(personagem.estaColidindo(inimigo)){
-    somAmbiente.stop();
-    somGameOver.play();
-    image(imgGameOver, ( width / 2 ) - 206, ( height / 2 ) - 39, 412, 78);
-    noLoop();
-  }
+
+  inimigos.forEach(inimigo => {
+    inimigo.exibe();
+    inimigo.move();
+    
+    if (personagem.estaColidindo(inimigo)) {
+      
+      //somAmbiente.stop();
+      //somGameOver.play();
+      
+      image(imgGameOver, width / 2 - 206, height / 2 - 90);
+      //colisao
+      //noLoop();
+      if (key === " ") {
+        somPulo = null;
+      }
+    }
+  });
 }
